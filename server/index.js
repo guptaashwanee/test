68f53fb007b021f3e184179f7fd54653fa733dd6
Ashwanee Kumar Gupta <ashwanee@anscer.com>
2024-07-15 15:04:51 +0530

    perf(server): express app innit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-console.log("Hello from Server")
+const express = require("express");
+const { createServer } = require("http");
+const app = express();
+
+const server = createServer(app);
+
+server.listen(9090, () => {
+  console.log("Listening on PORT: 9090");
+});
